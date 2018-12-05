e8a9f2cd88239570d7a693a32eb9097679cfb21b
Samir Baraily <samir.baraily@gmail.com>
2018-12-04 22:13:06 -0700

    Worked on controller, model amd View

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,10 +1,50 @@
 class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :update, :edit, :destroy]
   def index
+    @department = Department.all
   end
 
   def show
   end
 
   def new
+    @department = Department.new
+    render partial: "form"
   end
+
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to departments_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @department.update(department_params)
+      redirect_to departments_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @department.destroy
+    redirect_to departments_path
+  end
+
+  private
+    def department_params
+      params.require(:department).permit(:name)
+    end
+
+    def set_department
+      @department = Department.find(params[:id])
+    end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,50 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :update, :edit, :destroy]
   def index
+    @item = Item.all
   end
 
   def show
   end
 
   def new
+    @item = Item.new
+    render partial: "form"
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :price, :size)
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
 end
